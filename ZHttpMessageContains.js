e1f4078a59ac7c643396b6eeec5e0f493630c102
Crypt00o <octopus2672001@gmail.com>
2023-02-27 14:53:59 +0200

    update ZHttpMessageContains.js

--- ZHttpMessageContains.js
+++ ZHttpMessageContains.js
@@ -115,12 +115,8 @@ function httpMessageContains(utils,fuzzResult){
 }
 
 function showHttpMessageContainsResult(utils,fuzzResult){
-
-	  if (httpMessageContains(utils, fuzzResult)){
-			fuzzResult.addCustomState("Key Custom State", "Text Found");
-	  } else{
-			fuzzResult.addCustomState("Key Custom State", "Text Not Found");
-	  }
+	  const result = httpMessageContains(utils, fuzzResult)? "Text Found" : "Text Not Found" ;
+	  fuzzResult.addCustomState("Key Custom State", result);
 }
 
 
